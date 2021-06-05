cfa890942626dcb9dcf6dc2ca8d92d81900e9e53
himanthreddyl <85391023+himanthreddyl@users.noreply.github.com>
2021-06-05 16:46:39 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note(){
+return (
+  <div className="note">
+  <h1> javascript and react.js</h1>
+  <p>it is really an interesting session</p>
+  </div>
+);
+}
+
+export default Note;
